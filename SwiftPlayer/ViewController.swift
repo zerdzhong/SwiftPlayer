92b8aafb9a7677bf71c3ba925230026b75cf02d0
zhongzhendong <zhongzhendong@youku.com>
2016-03-10 17:15:27 +0800

    Initial Commit

---
+++ SwiftPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPlayer
+//
+//  Created by zhongzhendong on 16/3/10.
+//  Copyright © 2016年 zhongzhendong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
