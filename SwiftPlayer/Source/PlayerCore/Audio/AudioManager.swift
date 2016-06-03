9997923352f8abe79374361a17864f6ff46ec9d2
zerdzhong <zerd.zhong@gmail.com>
2016-06-03 21:03:44 +0800

    create AudioUint

--- SwiftPlayer/Source/PlayerCore/Audio/AudioManager.swift
+++ SwiftPlayer/Source/PlayerCore/Audio/AudioManager.swift
@@ -11,11 +11,19 @@ import AVFoundation
 
 enum AudioManagerError: ErrorType {
     case CategorySetError
+    case AudioUintCreateError
+    case StreamFormatGetError
+    case StreamFormatSetError
 }
 
 typealias AudioManagerOutputCallback = (data :Array<Float>, frameCount:Int, channelCount: Int) -> ()
 
 class AudioManager: NSObject {
+    
+    private var audioUint =  AudioUnit()
+    private var outputFormat = AudioStreamBasicDescription()
+    
+    var samplingRate: Float64 = 0
     
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self)
@@ -39,10 +47,40 @@ class AudioManager: NSObject {
         }catch {
             throw AudioManagerError.CategorySetError
         }
-         
+        
+        var status = OSStatus()
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(handleAudioRouteChange), name: AVAudioSessionRouteChangeNotification, object: nil)
         
         AVAudioSession.sharedInstance().addObserver(self, forKeyPath: "outputVolume", options: [.Initial, .New], context: nil);
+        
+        var description = AudioComponentDescription()
+        description.componentType = OSType(kAudioUnitType_Output)
+        description.componentSubType = OSType(kAudioUnitSubType_RemoteIO)
+        description.componentManufacturer = OSType(kAudioUnitManufacturer_Apple)
+        
+        let component = AudioComponentFindNext(nil, &description)
+        status = AudioComponentInstanceNew(component, &audioUint)
+        
+        if status != noErr {
+            throw AudioManagerError.AudioUintCreateError
+        }
+        
+        var size = UInt32(sizeof(AudioStreamBasicDescription))
+        
+        status = AudioUnitGetProperty(audioUint, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &outputFormat, &size)
+        
+        if status != noErr {
+            throw AudioManagerError.StreamFormatGetError
+        }
+        
+        outputFormat.mSampleRate = samplingRate;
+        
+        status = AudioUnitSetProperty(audioUint, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &outputFormat, size)
+        
+        if status != noErr {
+            throw AudioManagerError.StreamFormatSetError
+        }
     }
     
     func handleAudioRouteChange() -> Void {
